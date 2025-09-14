7f1c9f9beaa7a63836cf05a010280fcccf6d4f50
Franco Rodriguez <francogrch@gmail.com>
2025-09-14 19:04:39 -0300

    modificaciones

--- install.sh
+++ install.sh
@@ -43,34 +43,34 @@ function install() {
 function paquetes_adicionales() {
   read -p "Quieres instalar los paquetes de bluetooth? (y/N)" confirm
   if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
-    sudo pacman -S --noconfirm bluez bluez-utils
+    install bluez bluez-utils
   fi
 
   read -p "Quieres instalar los paquetes de edicion de imagen y video? (y/N)" confirm
   if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
-    sudo pacman -S --noconfirm kdenlive gimp obs-studio
+    install kdenlive gimp obs-studio
   fi
 
   read -p "Quieres instalar los paquetes de ofimatica? (y/N)" confirm
   if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
-    sudo pacman -S --noconfirm libreoffice-fresh
+    install libreoffice-fresh
   fi
 
   read -p "Quieres instalar los paquetes de virtualizacion? (y/N)" confirm
   if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
-    sudo pacman -S --noconfirm virtualbox virtualbox-host-modules-arch
+    install virtualbox virtualbox-host-modules-arch
     sudo modprobe vboxdrv
     sudo gpasswd -a $USER vboxusers
   fi
 
   read -p "Quieres instalar los paquetes de redes? (y/N)" confirm
   if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
-    sudo pacman -S --noconfirm networkmanager network-manager-applet
+    install networkmanager network-manager-applet
   fi
 
   read -p "Quieres instalar los paquetes de gaming? (y/N)" confirm
   if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
-    sudo pacman -S --noconfirm steam lutris
+    install steam lutris
   fi
 }
 
@@ -78,12 +78,15 @@ function paquetes_adicionales() {
 sudo pacman -Syu
 read -p "Actualizacion completada. Presiona Enter para continuar..."
 clear
+
 read -p "Quieres instalar los paquetes basicos? (y/N)" confirm
 if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
   install git base-devel vim neovim wget htop ripgrep fastfetch zip unzip xclip xdotool man-db xdg-user-dirs xorg-xrandr lightdm lightdm-gtk-greeter bspwm sxhkd picom feh dunst polybar alacritty kitty rofi thunar firefox viewnior maim pulseaudio pulsemixer pulseaudio-alsa xsettingsd materia-gtk-theme papirus-icon-theme redshift polkit-gnome xcolor
 fi
+
 read -p "Instalacion completada. Presiona Enter para continuar..."
 clear
+
 read -p "Quieres instalar algun paquete adicional? (y/N)" confirm
 if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
   paquetes_adicionales
@@ -92,7 +95,7 @@ fi
 echo "¡Instalación de paquetes completada!"
 read -p "Presiona Enter para continuar..."
 clear
-# Habilitar servicios con sudo
+
 systemctl --user enable pulseaudio.service
 sudo systemctl enable bluetooth.service
 sudo systemctl enable lightdm.service
@@ -101,12 +104,14 @@ sudo chown -R "$USER":"$USER" "$HOME/"
 echo "¡Servicios habilitados!"
 read -p "Presiona Enter para continuar..."
 clear
+
 # Comandos de configuracion de usuario (sin sudo)
 mkdir -p "$HOME/.config"
 cp -rf ./dotfiles/.config/* "$HOME/.config"
 cp -rf ./dotfiles/.xsettingsd "$HOME/"
 mkdir -p "$HOME/Pictures"
 cp ./dotfiles/Pictures/wallpaper.png "$HOME/Pictures"
+cp ./dotfiles/Pictures/wallpaper.jpg "$HOME/Pictures"
 chmod +x "$HOME/.config/bspwm/bspwmrc"
 chmod +x "$HOME/.config/sxhkd/sxhkdrc"
 mkdir -p "$HOME/.local/share/icons/dunst"
@@ -115,14 +120,16 @@ cp ./dotfiles/icons/* "$HOME/.local/share/icons/dunst"
 echo "¡Instalación de dotfiles completada!"
 read -p "Presiona Enter para continuar..."
 clear
-sudo pacman -S --noconfirm ttf-iosevka-nerd ttf-jetbrains-mono noto-fonts-emoji ttf-nerd-fonts-symbols ttf-firacode-nerd ttf-hack-nerd
+
+echo "¡Instalando Fuentes!"
+install ttf-iosevka-nerd ttf-jetbrains-mono noto-fonts-emoji ttf-nerd-fonts-symbols ttf-firacode-nerd ttf-hack-nerd
 echo "¡Instalación de fuentes completada!"
 read -p "Presiona Enter para continuar..."
 clear
 
 read -p "Quieres instalar mi zsh personal? (y/N)" confirm
 if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
-  sudo pacman -S --noconfirm zsh zoxide zsh-autocomplete zsh-autosuggestions zsh-syntax-highlighting fzf bat tree fd
+  install zsh zoxide zsh-autocomplete zsh-autosuggestions zsh-syntax-highlighting fzf bat tree fd
   rm -rf "$HOME/.oh-my-zsh"
   clear
   echo "Cuando termine de instalar Oh My Zsh, escribe exit para cerrar la terminal y volver a este script."
