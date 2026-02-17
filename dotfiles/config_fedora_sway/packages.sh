26ffba2518bae48f36365a7e0ad075181768426f
Franco <francogrch@gmail.com>
2026-02-17 14:17:30 -0300

    Update requeriments fedora sway

--- dotfiles/config_fedora_sway/packages.sh
+++ dotfiles/config_fedora_sway/packages.sh
@@ -1,5 +1,32 @@
-alacritty wofi blueman-manager wlogout
+sudo dnf install alacritty wofi blueman-manager wlogout waybar zsh neovim zsh zoxide zsh-autosuggestions zsh-syntax-highlighting fzf bat tree fd lsd docker uv
 
-cd .config/sway/scripts
+chsh -s $(which zsh)
+
+cd .config/sway
+sudo dnf install python3-pip
 python3 -m pip install --user tendo i3ipc
+
+Fuente: Iosevka Nerd Font
+mkdir -p ~/.local/share/fonts
+cp ~/Descargas/mi-fuente.ttf ~/.local/share/fonts/
+fc-cache -fv
+
+
+vesktop
+dbdeader
+vscode
+oh-my-zsh
+powerlevel10k
+
+git clone zsh-autocomplete
+cp -r zsh-autocomplete /usr/share
+
+## Dark mode 
+nvim ~/.config/gtk-3.0/settings.ini
+
+[Settings]
+gtk-application-prefer-dark-theme=1
+gtk-theme-name=Adwaita
+
+
 
